649f248e62a9c8ff84b53c06c34a4a0544707922
lkhang3000 <leh912947@gmail.com>
2025-10-26 18:10:14 +0700

    adjust code for frontend testing

--- website/website/settings.py
+++ website/website/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "website",
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "localhost",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
