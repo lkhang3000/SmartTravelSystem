ccda7f5618a9c63c47b0c1c6334b877558279c42
lkhang3000 <leh912947@gmail.com>
2025-12-14 03:43:54 +0700

    update database and pics

--- create_mysql_db.sql
+++
@@ -1,7 +0,0 @@
--- Tạo database cho SmartTravel
-CREATE DATABASE IF NOT EXISTS smarttravel_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Nếu cần tạo user mới (tùy chọn)
--- CREATE USER 'smarttravel_user'@'localhost' IDENTIFIED BY 'your_password';
--- GRANT ALL PRIVILEGES ON smarttravel_db.* TO 'smarttravel_user'@'localhost';
--- FLUSH PRIVILEGES;
