4d293b71b306956f291e88e394269aafc2b295b4
hoankhtn <hoanbk006@gmail.com>
2025-12-14 13:42:42 +0700

    Backend email profile

--- website/sightseeing/urls.py
+++ website/sightseeing/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("change-email/", views.change_email, name="change_email"),
     # Authentication URLs
     path('login/', views.login_page, name='login_page'),
     path('signup/', views.signup_page, name='signup_page'),
